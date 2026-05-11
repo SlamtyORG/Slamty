42eebcde026516bfd642dd5d2357872e2f3e1f02
حازم <hazem415369@fci.bu.edu.eg>
2026-05-11 15:54:35 +0300

    add report entity

--- Slamty.Domain/Entities/Report.cs
+++ Slamty.Domain/Entities/Report.cs
@@ -2,5 +2,9 @@
 {
     public class Report : BaseEntity
     {
+        public double Lat { get; set; }
+        public double Lng { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsNow { get; set; }
     }
 }
