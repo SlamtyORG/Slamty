6cca3c4ca0b537a596b538af93f5624cc25bc05d
ahmed-1 <ahmedalibahaa2003@gmail.com>
2026-05-14 14:27:14 +0300

    Adding Reset password and forget password feature

---
+++ Slamty.Application/Auth/Commands/ForgetPassword/ForgetPasswordCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Slamty.Application.ResponseTypes;
+
+namespace Slamty.Application.Auth.Commands.ResetPassword
+{
+    public record ForgetPasswordCommand(string email) : IRequest<ResponseResult<bool>>;
+
+}
