3c521b6328ba5f58acd5af4904b6f93224787056
Ahmed  Ali <132076539+AhmedAliDL@users.noreply.github.com>
2026-05-14 14:35:10 +0300

    Merge pull request #12 from SlamtyORG/resetPassword
    Adding Reset password and forget password feature

---
+++ Slamty.Application/Auth/Commands/ResetPassword/ResetPasswordCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Slamty.Application.Auth.Dtos;
+using Slamty.Application.ResponseTypes;
+
+namespace Slamty.Application.Auth.Commands.ResetPassword
+{
+    public record ResetPasswordCommand(ResetPasswordDto ResetPasswordDto) : IRequest<ApiResponse<bool>>;
+
+
+}
---
+++ Slamty.Application/Auth/Commands/ResetPassword/ResetPasswordCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Slamty.Application.Auth.Commands.ResetPassword
+{
+    public class ResetPasswordCommandValidator : AbstractValidator<ResetPasswordCommand>
+    {
+        public ResetPasswordCommandValidator()
+        {
+            RuleFor(r => r.ResetPasswordDto.UserEmail)
+                .NotEmpty()
+                .WithMessage("Email is required")
+                .MinimumLength(15)
+                .WithMessage("Email must be greater than 15");
+            RuleFor(r => r.ResetPasswordDto.ValidationToken)
+                .NotEmpty()
+                .WithMessage("ValidationToken is required");
+
+
+        }
+    }
+
+
+}
