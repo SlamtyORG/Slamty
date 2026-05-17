136f2cc33b2c1e9d0b15712b9424404c3cb4194f
ahmed-1 <ahmedalibahaa2003@gmail.com>
2026-05-17 19:41:58 +0300

    Verify OTP

---
+++ Slamty.Application/Auth/Commands/SendOTP/SendOTPCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Slamty.Application.Auth.Dtos;
+using Slamty.Application.Interfaces.Servicese;
+using Slamty.Application.ResponseTypes;
+using Slamty.Domain.Entities;
+
+namespace Slamty.Application.Auth.Commands.SendOTP
+{
+    public class SendOTPCommandHandler : IRequestHandler<SendOTPCommand, ApiResponse<bool>>
+    {
+        private readonly IEmailSenderService _emailSenderService;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IConfiguration _config;
+        public SendOTPCommandHandler(IEmailSenderService emailSenderService, UserManager<AppUser> userManager, IConfiguration config)
+        {
+            _emailSenderService = emailSenderService;
+            _userManager = userManager;
+            _config = config;
+        }
+
+        public async Task<ApiResponse<bool>> Handle(SendOTPCommand request, CancellationToken cancellationToken)
+        {
+
+            var user = await _userManager.FindByEmailAsync(request.EmailAddress);
+
+            if (user == null)
+                return new ApiResponse<bool>(System.Net.HttpStatusCode.NotFound, false, "User not found.");
+
+            var otp = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+            return await _emailSenderService.SendEmailService(
+                _config["SmtpSettings:AdminEmail"]!,
+                request.EmailAddress,
+                new EmailSenderDto
+                {
+                    Subject = "Email Confirmation Code",
+                    Body = $"Your confirmation code is: {otp}"
+                });
+
+        }
+    }
+
+}
---
+++ Slamty.Application/Auth/Commands/VerifyOTP/VerifyOTPCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Slamty.Application.ResponseTypes;
+using Slamty.Domain.Entities;
+
+namespace Slamty.Application.Auth.Commands.VerifyOTP
+{
+    public class VerifyOTPCommandHandler : IRequestHandler<VerifyOTPCommand, ApiResponse<bool>>
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public VerifyOTPCommandHandler(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<ApiResponse<bool>> Handle(VerifyOTPCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByEmailAsync(request.OTPVerificationDto.EmailAddress);
+
+            if (user == null)
+                return new ApiResponse<bool>(System.Net.HttpStatusCode.NotFound, false, "User not found.");
+
+            var otpChecker = await _userManager.ConfirmEmailAsync(user, request.OTPVerificationDto.OTP);
+
+            if (!otpChecker.Succeeded)
+                return new ApiResponse<bool>(System.Net.HttpStatusCode.Unauthorized, false, "OTP is wrong.");
+
+            user.EmailConfirmed = true;
+            await _userManager.UpdateAsync(user);
+
+            return new ApiResponse<bool>(System.Net.HttpStatusCode.OK, true, "Email is confirmed");
+        }
+    }
+
+
+}
