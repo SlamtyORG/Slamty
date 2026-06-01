00ae14ec34ab8e5cb1c875da82a50e55a5886d63
حازم <hazem415369@fci.bu.edu.eg>
2026-06-02 02:13:45 +0300

    Add Get Notification End-point close #24

---
+++ Slamty.Application/Features/Home/Queries/GetNotifications/GetNotificationsQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Slamty.Application.Features.Home.Queries.GetNotifications
+{
+    public class GetNotificationsQueryValidator : AbstractValidator<GetNotificationsQuery>
+    {
+        public GetNotificationsQueryValidator()
+        {
+            RuleFor(x => x.UserId)
+                .NotEmpty().WithMessage("UserId is required.")
+                .Must(userId => Guid.TryParse(userId, out _)).WithMessage("UserId must be a valid GUID.");
+        }
+    }
+}
---
+++ Slamty.Domain/Specifications/GetNotificationByUserIdSpec.cs
@@ -0,0 +1,12 @@
+﻿using Slamty.Domain.Entities;
+
+namespace Slamty.Domain.Specifications
+{
+    public class GetNotificationByUserIdSpec : BaseSpecification<Notification>
+    {
+        public GetNotificationByUserIdSpec(string userId) : base(n => n.UserId == userId)
+        {
+            OrderBy = n => n.Date;
+        }
+    }
+}
