e23e4a8bb4c3db0860c17f8d370e057138bb8a46
Youssef-Ebn-Yasser <yh29152@gmail.com>
2026-06-05 19:36:00 +0300

    Create report close #33

---
+++ Slamty.Api/Controllers/Mobile/ReportsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Slamty.Application.Features.Reports.Commands.UpdateReport;
+
+namespace Slamty.Api.Controllers.Mobile;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ReportsController : BaseMobileApiController
+{
+    [HttpPut("UpdateReport")]
+    public async Task<IActionResult> UpdateReport(UpdateReportCommand request)
+    {
+        var response = await Mediator.Send(request);
+        return HandleResult(response);
+    }
+}
